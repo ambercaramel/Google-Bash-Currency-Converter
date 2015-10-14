d752cf342cde0db55ecb058648fb4e324746efbd
Bill <superbill81@hotmail.com>
2015-10-14 13:27:52 +0100

    regex corrections
    corrected regexes

--- gbcc.sh
+++ gbcc.sh
@@ -2,8 +2,8 @@
 wget https://currency-api.appspot.com/api/BTC/GBP.json?amount=0.3131 -o wgetlog  
 # -o surpresses output to stderror and creates a log
 # the log should get replaced each time rather than grow in size
-sed 's/{"success":true,"source":"BTC","target":"GBP","rate":164.0811,"amount"://' GBP.json?amount=0.3131 > tmp1
-sed 's/,"message":""}//' tmp1 >tmp2
+sed 's/^{.*.amount"://' GBP.json?amount=0.3131 > tmp1
+sed 's/,"m.*//' tmp1 >tmp2
 # sed aka stream editor chops off the useless stuff, essentially parsing the downloading string.
 echo -n £
 # -n prevents a newline, so we can get the desired display e.g. £150.00 rather than £ on one line and 150.00 on the next
